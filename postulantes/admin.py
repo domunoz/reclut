0e910c6d96ca4e3ba9e27578de3d582b47431cd2
Fernando Muñoz <fernandomunoztoledo@yahoo.com>
2014-11-06 08:38:50 -0300

    changes to admin.py

--- postulantes/admin.py
+++ postulantes/admin.py
@@ -4,18 +4,18 @@ from models import Postulante, Instalacion
 # Register your models here.
 class PostulanteAdmin(admin.ModelAdmin):
     list_display= ( 'nombre_completo',  'comuna',
-          'fecha', 'medio',   'ha_sido_condenado_o_detenido', 'industrial','contratado', 'creado_por'  )
+          'fecha',   'ha_sido_condenado_o_detenido', 'industrial','contratado',  'observaciones', 'medio',  )
     list_filter =  (  'contratado', 'industrial', 'ha_sido_condenado_o_detenido',   'fecha', 'comuna',  'creado_por',)
 #    list_editable = ('ha_sido_condenado_o_detenido',)
     search_fields = ('nombres', 'apellidos', 'rut', 'observaciones') 
     fieldsets = (
         ('', {'fields': ('fecha', 'medio')}),
         ('Información personal', {'fields': ('nombres','apellidos', 'rut','fecha_de_nacimiento', 'nacionalidad',
-        'sexo', 'escolaridad', ('estado_civil', 'hijos'), 'jubilado', 'afp', 'sistema_de_salud')}),
+        'sexo', 'escolaridad', )}),
         ('Información de contacto', {'fields': ('domicilio', 'comuna', 'email', 'telefono',
         'telefono_emergencia')}),
         ('Otros', {'fields': (  ('ha_sido_condenado_o_detenido', 'motivo'), 'industrial' ,
-         'contratado', ('os10', 'vencimiento'), 'observaciones' , )}),
+         ('contratado', 'fecha_contratacion'), 'instalacion', ('os10', 'vencimiento'), 'observaciones' , )}),
          #('fecha_contratacion', 'instalacion')#
     )
 
