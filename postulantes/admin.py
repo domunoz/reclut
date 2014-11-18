b12600524dcdf7381371519abccac905a9483991
fernando muñoz <fernandomunoztoledo@gmail.com>
2014-11-18 13:10:24 -0300

    edit administrator

--- postulantes/admin.py
+++ postulantes/admin.py
@@ -6,7 +6,7 @@ from models import Postulante, Instalacion, Contratado, Supervisor, Cliente, Com
 class PostulanteAdmin(admin.ModelAdmin):
     list_display= ( 'fecha', 'medio', 'medio1', 'nombres', 'apellidos',  'comuna', 'ha_sido_condenado_o_detenido', 'industrial', 
              'contratado', )#  'observaciones',  )
-    list_filter =  (  'contratado',  ('fecha', DateRangeFilter),  'medio', 'comuna')
+    list_filter =  (  'contratado',  ('fecha', DateRangeFilter),  'medio1', 'comuna')
 #    list_editable = ('ha_sido_condenado_o_detenido',)
     list_editable = ('medio', 'medio1',)
 
