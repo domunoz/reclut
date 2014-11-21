cab783448cca60daba76ce7a7970f7788d9929e3
fernando muñoz <fernandomunoztoledo@gmail.com>
2014-11-21 18:00:51 -0300

    calcula postulantes por cada medio

--- postulantes/admin.py
+++ postulantes/admin.py
@@ -4,12 +4,10 @@ from daterange_filter.filter import DateRangeFilter
 from models import Postulante, Instalacion, Contratado, Supervisor, Cliente, Comuna, Region, Medio
 # Register your models here.
 class PostulanteAdmin(admin.ModelAdmin):
-    list_display= ( 'fecha', 'medio1', 'nombres', 'apellidos',  'comuna', 'ha_sido_condenado_o_detenido', 'industrial', 
-             'contratado', )#  'observaciones',  )
-    list_filter =  (  'contratado',  ('fecha', DateRangeFilter),  'medio1', 'comuna')
+    list_display= ( 'fecha', 'medio1', 'nombres', 'apellidos',  'comuna', 'ha_sido_condenado_o_detenido', 'industrial', 'contratado', )#  'observaciones',  ) list_filter =  (  'contratado',  ('fecha', DateRangeFilter),  'medio1', 'comuna')
 #    list_editable = ('ha_sido_condenado_o_detenido',)
 #    list_editable = ('medio', 'medio1',)
-
+    list_filter = ('contratado', ('fecha', DateRangeFilter), 'medio1', 'comuna')
 #    radio_fields = {'sexo': admin.VERTICAL, 'escolaridad': admin.HORIZONTAL }
 
     search_fields = ('nombres', 'apellidos', 'rut',) 
@@ -32,6 +30,19 @@ class ContratadoAdmin(admin.ModelAdmin):
     )
     list_filter = ('os10', 'fecha_contratacion')
 
+
+class MedioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'total_postulantes', 'contratados', )
+
+    def total_postulantes(self, obj):
+   #     return obj.nombre
+        return Postulante.objects.filter(medio1=obj).count()
+
+    def contratados(self, obj):
+        return Postulante.objects.filter(medio1=obj, contratado=True).count()       
+      
+
+
 admin.site.register(Postulante, PostulanteAdmin)
 admin.site.register(Instalacion)
 #admin.site.register(Contratado)
@@ -39,6 +50,6 @@ admin.site.register(Supervisor)
 admin.site.register(Cliente)
 admin.site.register(Comuna)
 admin.site.register(Region)
-admin.site.register(Medio)
+admin.site.register(Medio, MedioAdmin)
 
 admin.site.register(Contratado, ContratadoAdmin)
